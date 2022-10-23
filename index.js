97c845bcab8382ad850562d86c941862fecd3e14
leeducanh03 <leeducanh03@gmail.com>
2022-10-23 19:16:44 +0700

    add

---
+++ index.js
@@ -0,0 +1,226 @@
+let data = [
+    {
+        image: "./img/samsung-galaxy-z-fold-2-123620-093621-400x460.png",
+        name: "SAMSUNG GALAXY Z FOLD 2",
+        price: "23.000.000 VNĐ"
+    },
+    {
+        image: "./img/ip11pr.jpg",
+        name: "IPHONE 11PRO",
+        price: "33.000.000 VNĐ"
+    },
+    {
+        image: "./img/IP 13 PRO.webp",
+        name: "IPHONE 13PRO",
+        price: "45.000.000 VNĐ"
+    },
+    {
+        image: "./img/IP 13MINI.webp",
+        name: "IPHONE 13 MINI",
+        price: "35.000.000 VNĐ"
+    },
+    {
+        image: "./img/IP 13PRM.webp",
+        name: "IPHONE 13 PRO MAX",
+        price: "55.000.000 VNĐ"
+    },
+    {
+        image: "./img/IP 14 PRO.webp",
+        name: "IPHONE 14 PRO",
+        price: "47.000.000 VNĐ"
+    },
+    {
+        image: "./img/IP 14.webp",
+        name: "IPHONE 14",
+        price: "43.000.000 VNĐ"
+    },
+    {
+        image: "./img/IP 14PL.webp",
+        name: "IPHONE 14 PLUS",
+        price: "40.000.000 VNĐ"
+    },
+    {
+        image: "./img/IP 14PRM.webp",
+        name: "IPHONE 14 PRO MAX",
+        price: "65.000.000 VNĐ"
+    },
+    {
+        image: "./img/ip11pr.jpg",
+        name: "IPHONE 11 PRO",
+        price: "30.000.000 VNĐ"
+    },
+    {
+        image: "./img/ip8s.jpg",
+        name: "IPHONE 8S",
+        price: "20.000.000 VNĐ"
+    },
+    {
+        image: "./img/iphone-7-plus-128gb-de-400x460.png",
+        name: "IPHONE 7 PLUS",
+        price: "15.000.000 VNĐ"
+    },
+    {
+        image: "./img/iphone-se-2020-do-400x460-400x460.png",
+        name: "IPHONE SE",
+        price: "10.000.000 VNĐ"
+    },
+    {
+        image: "./img/iphone-xs-gold-400x460-1-400x460.png",
+        name: "IPHONE XS GOLD",
+        price: "12.000.000 VNĐ"
+    },
+    {
+        image: "./img/ipxr128gb'.jpg",
+        name: "IPHONE XR",
+        price: "13.000.000 VNĐ"
+    },
+    {
+        image: "./img/samsung-galaxy-a21s-055620-045627-400x460.png",
+        name: "SAMSUNG GALAXY A21S",
+        price: "10.000.000 VNĐ"
+    },
+    {
+        image: "./img/samsung-galaxy-note-20-ultra-vangdong-400x460-400x460.png",
+        name: "SAMSUNG GALAXY NOTE 20 ULTRA",
+        price: "13.000.000 VNĐ"
+    },
+    {
+        image: "./img/samsung-galaxy-z-flip-chitiet-2-788x544.png",
+        name: "SAMSUNG GALAXY Z FLIP",
+        price: "9.000.000 VNĐ"
+    },
+    {
+        image: "./img/samsung-galaxy-z-fold-2-123620-093621-400x460.png",
+        name: "SAMSUNG GALAXY Z FOLD",
+        price: "8.000.000 VNĐ"
+    },
+    {
+        image: "./img/samsung-galaxy-a33-5g-500x500.jpg",
+        name: "SAMSUNG GALAXY A33",
+        price: "7.000.000 VNĐ"
+    },
+    {
+        image: "./img/apple-macbook-air-mqd32sa-a-i5-5350u-600x600.jpg",
+        name: "APPLE MACBOOK ARI",
+        price: "40.000.000 VNĐ"
+    },
+    {
+        image: "./img/asus tuf gaming.webp",
+        name: "ASUS TUF GAMING",
+        price: "25.000.000 VNĐ"
+    },
+    {
+        image: "./img/asus vivobook.webp",
+        name: "ASUS VIVOBOOK",
+        price: "27.000.000 VNĐ"
+    },
+    {
+        image: "./img/asus zenbook.webp",
+        name: "ASUS ZENBOOK",
+        price: "30.000.000 VNĐ"
+    },
+    {
+        image: "./img/asus-vivobook-gaming-f571gt-i7-al858t-226256-600x600.jpg",
+        name: "ASUS VIVOBOOK GAMING",
+        price: "35.000.000 VNĐ"
+    },
+    {
+        image: "./img/DELL GAMING.webp",
+        name: "DELL GAMING",
+        price: "50.000.000 VNĐ"
+    },
+    {
+        image: "./img/hp-envy-13-ba0046tu-i5-171m7pa-225859-600x600.jpg",
+        name: "HP ENVY 13",
+        price: "25.000.000 VNĐ"
+    },
+    {
+        image: "./img/Laptop Dell Inspiron.webp",
+        name: "LAPTOP DELL INSPIRON",
+        price: "35.000.000 VNĐ"
+    },
+    {
+        image: "./img/Laptop Dell Latitude.webp",
+        name: "LAPTOP DELL LATITUDE",
+        price: "37.000.000 VNĐ"
+    },
+    {
+        image: "./img/Laptop Dell Vostro.webp",
+        name: "LAPTOP DELL VOSTRO",
+        price: "40.000.000 VNĐ"
+    },
+    {
+        image: "./img/lenovo-ideapad-3-15iil05-i3-81we003rvn-013920-053901-600x600.jpg",
+        name: "LENOVO IDEAPAD 3",
+        price: " 29.000.000 VNĐ"
+    },
+    {
+        image: "./img/laptop-hp-pavilion-15-eg0513tu-46m12pa_9174fc105c8a4db9af7dfc29b33b4769_large.webp",
+        name: "LAPTOP HP PAVILION",
+        price: "11.500.000 VNĐ"
+    },
+    {
+        image: "./img/laptop-msi-modern-14-b11mou-1028vn_1a6c3972f3de4fbc9070eca8914ffa55_large.webp",
+        name: "LAPTOP MSI MODERN",
+        price: "15.000.000 VNĐ"
+    },
+    {
+        image: "./img/laptop_acer_aspire_3_a315_58_3939_05f238da8e5b46e893b725d5eaf5f8ca_large.webp",
+        name: "LAPTOP ACER ASPIRE",
+        price: "12.900.000 VNĐ",
+    },
+    {
+        image: "./img/lenovo-ideapad-3-15iil05-i3-81we003rvn-013920-053901-600x600.jpg",
+        name: "LENOVO IDEAPAD 3 15II105 I3",
+        price: "9.000.000 VNĐ"
+    },
+]
+
+let result = "";
+function showData() {
+    let listItem = document.getElementById("showData");
+    for (let i = 0; i < data.length; i++) {
+        result += `
+       <div class="col-2th" style = "margin-bottom: 30px;">
+            <div class="card" >
+                <div class="image"   >
+                    <img style = "margin-left: 30px;" src="${data[i].image}">
+                </div>
+                <div class="name">
+                    <p style = "text-align:center;">${data[i].name}</p>
+                </div>
+                <div class="price">
+                    <p style = "text-align:center;">${data[i].price}</p>
+                </div>
+            </div>
+        </div>
+       `
+    }
+    listItem.innerHTML = result;
+}
+showData();
+function search() {
+    let result="";
+    let inputValue=document.getElementById("input").value.toUpperCase();
+    let listItem = document.getElementById("showData");
+    for (let i = 0; i<data.length; i++) {
+        if(data[i].name.includes(inputValue)){
+            result += `
+            <div class="col-2th" style = "margin-bottom: 30px;">
+                 <div class="card" >
+                     <div class="image"   >
+                         <img style = "margin-left: 30px;" src="${data[i].image}">
+                     </div>
+                     <div class="name">
+                         <p style = "text-align:center;">${data[i].name}</p>
+                     </div>
+                     <div class="price">
+                         <p style = "text-align:center;">${data[i].price}</p>
+                     </div>
+                 </div>
+             </div>
+            `
+        }
+    }
+    listItem.innerHTML = result;
+}
